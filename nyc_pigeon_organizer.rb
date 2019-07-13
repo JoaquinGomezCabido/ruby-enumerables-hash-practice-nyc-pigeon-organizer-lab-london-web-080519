b1a5e9b83e366224321fef32daf6ba3ab2254476
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-13 16:10:30 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,22 +1,21 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |key, value|
+    value.each do |new_value, names|
+      names.each do |name|
 
-    names_array = data[:gender].values.flatten.uinq
-    object = Hash.new
-
-    names_array.each do |item|
-      object[item] = {}
-    end
-
-    object.keys.each do |item|
-      data.keys.each do |element|
-        elements.values.each do |a|
-          if object[item].includes?(elements.a)
-            object[item][element].push(elements.a)
-          end
+        if !new_hash[name]
+          new_hash[name] = {}
         end
+
+        if !new_hash[name][key]
+          new_hash[name][key] = []
+        end
+
+        new_hash[name][key] << new_value.to_s
+
       end
     end
-
-    object
+  end
+  new_hash
 end
